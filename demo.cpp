3e56b869efd72c25157b1eb1b8d68abb0fec22e3
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-07-13 22:14:57 +0530

    created delcartion for semaphore and mutex log with documentation

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ASUS on 13-07-2024.
+//
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+cout<<"hello world";
+}
--- proxy_Server_with_Cache.c
+++ proxy_Server_with_Cache.c
@@ -6,7 +6,10 @@
 #include <stdio.h>
 #include <string.h>
 #include<time.h>
+#include <pthread.h>
+
 typedef struct cache_element cache_element;
+#define MAX_CLIENT 10
 
 struct cache_element
 {
@@ -17,3 +20,36 @@ struct cache_element
     cache_element *next;
 };
 
+
+// Method to find the cache with the associated URL  in cache linklist
+cache_element* find(char* url);
+
+//Method to add the receiveing data from WWW into cache linklist
+int add_cache_element(char* data, int size, char* url);
+
+// Method to remove element from cache linklist
+void remove_cache_element();
+
+// Port number on which our Proxy server is running
+int port_number= 8080;
+
+
+
+int proxy_socketId;
+
+pthread_t tid[MAX_CLIENT];
+
+sem_t semaphore;
+
+//lock for handing shared resource - here LRU cache is shared resources and when multiple thread   try to write it in RACE CONDITION occurs
+//lock has only 2 value  -   0 means resource not accuired, 1 mean resource is currently accuired
+//                           Story :)
+// at first a thread will come to write data from WWW to LRU cache, it check wether the cache is 
+// ocupied or not with help of lock, if not ocuppied it will accire the resource and set LOCK to 1 and
+// when work is over it setback LOCK to 0
+
+// is us to handle shared resources we uses mutex
+pthread_mutex_t lock;
+
+cache_element* head;
+int cache_size;
