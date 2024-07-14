c83be891182767b26377ae3388fd3b008b7b1e3b
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-07-14 12:36:16 +0530

    Setting up the proxy server and sockets

--- proxy_Server_with_Cache.c
+++ proxy_Server_with_Cache.c
@@ -67,3 +67,82 @@ pthread_mutex_t lock;
 
 cache_element* head;
 int cache_size;
+
+
+
+
+
+
+int main(int argc, char * argv[])
+{
+    // This is use to store socket id and lenth of address of client
+    int client_socketId, client_len;
+
+    struct sockaddr_in server_addr, client_addr;
+
+    //Intializing semaphore with address , min and max vaalue
+    sem_init(&semaphore,0, MAX_CLIENT);
+    pthread_mutex_init(&lock, NULL);
+
+    if(argv ==2)
+    {
+        port_number = atoi(argv[1]);
+    }
+    else
+    {
+        printf("Too less argument provided");
+        exit(1);
+    }
+
+    
+
+
+
+
+    printf("Starting Proxy Server at port: %d\n",port_number);
+
+    //This is the main and only Proxy server socket ,we need to creates socket id of proxy server that listens
+    //incomming request --> if req accepts   --> then a new thread is spawn    an socket is open
+    proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+
+	if( proxy_socketId < 0)
+	{
+		perror("Failed to create socket.\n");
+		exit(1);
+	}
+
+    // This is only socket that listen for connection request so, we have to ad the functio nality to use it again , for different client
+
+
+    int reuse =1;
+	if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) 
+        perror("setsockopt(SO_REUSEADDR) failed\n");
+
+    //set the value to zero , bydefault in c garbage value is stored
+    bzero((char*)&server_addr, sizeof(server_addr));  
+
+    server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_number); // Assigning port to the Proxy
+	server_addr.sin_addr.s_addr = INADDR_ANY; // Any available adress assigned
+
+
+
+    //Binding the socket
+	if( bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0 )
+	{
+		perror("Port is not free\n");
+		exit(1);
+	}
+	printf("Binding on port: %d\n",port_number);
+
+
+
+    //// Proxy socket listening to the requests - this code setup proxy server to listen
+	int listen_status = listen(proxy_socketId, MAX_CLIENTS);
+
+	if(listen_status < 0 )
+	{
+		perror("Error while Listening !\n");
+		exit(1);
+	}
+}
