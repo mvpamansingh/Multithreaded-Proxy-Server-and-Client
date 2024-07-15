e97d6cb1c6d3f380e5c1ab984e28ceea562452c7
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-07-15 23:36:58 +0530

    Checking the version of HTTP protocol used by client

--- proxy_Server_with_Cache.c
+++ proxy_Server_with_Cache.c
@@ -573,3 +573,44 @@ int connectRemoteServer(char* host_addr, int port_num)
 	// free(host_addr);
 	return remoteSocket;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int checkHTTPversion(char *msg)
+{
+	int version = -1;
+
+	if(strncmp(msg, "HTTP/1.1", 8) == 0)
+	{
+		version = 1;
+	}
+	else if(strncmp(msg, "HTTP/1.0", 8) == 0)			
+	{
+		version = 1;										// Handling this similar to version 1.1
+	}
+	else
+		version = -1;
+
+	return version;
+}
